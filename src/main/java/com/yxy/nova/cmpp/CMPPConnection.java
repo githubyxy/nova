37e11ec88064040c674abbd0582a23ba33fd848b
yuxiaoyu <2416175338@qq.com>
2020-12-03 15:37:08 +0800

    modify

--- src/main/java/com/yxy/nova/cmpp/CMPPConnection.java
+++ src/main/java/com/yxy/nova/cmpp/CMPPConnection.java
@@ -76,7 +76,7 @@ public class CMPPConnection extends PSocketConnection
         try
         {
 //            Resource resource = new Resource(getClass(), "resource");
-            Resource resource = new Resource( getClass().getClassLoader().getResource("cmpp/resource").toString());
+            Resource resource = new Resource( getClass().getClassLoader().getResource("cmpp/resource.xml").toString());
             return resource;
         }
         catch(IOException e)
