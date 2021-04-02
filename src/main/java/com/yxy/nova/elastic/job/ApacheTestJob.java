08536408cecc9e6d214cf937a38dc2fcf7b15744
yuxiaoyu <2416175338@qq.com>
2021-04-02 16:46:26 +0800

    apacheTestJob

--- src/main/java/com/yxy/nova/elastic/job/ApacheTestJob.java
+++ src/main/java/com/yxy/nova/elastic/job/ApacheTestJob.java
@@ -1,21 +1,21 @@
-package com.yxy.nova.elastic.job;
-
-import com.alibaba.fastjson.JSON;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.elasticjob.api.ShardingContext;
-import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
-
-/**
- * @author yuxiaoyu
- * @date 2021/3/29 下午2:21
- * @Description
- */
-@Slf4j
-public class ApacheTestJob implements SimpleJob {
-
-    @Override
-    public void execute(ShardingContext shardingContext) {
-        log.info(JSON.toJSONString(shardingContext));
-    }
-
-}
+//package com.yxy.nova.elastic.job;
+//
+//import com.alibaba.fastjson.JSON;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.shardingsphere.elasticjob.api.ShardingContext;
+//import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
+//
+///**
+// * @author yuxiaoyu
+// * @date 2021/3/29 下午2:21
+// * @Description
+// */
+//@Slf4j
+//public class ApacheTestJob implements SimpleJob {
+//
+//    @Override
+//    public void execute(ShardingContext shardingContext) {
+//        log.info(JSON.toJSONString(shardingContext));
+//    }
+//
+//}
