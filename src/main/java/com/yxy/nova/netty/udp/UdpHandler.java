3e5f90832ba05b6e9001d936f06209101d15b552
yuxiaoyu <2416175338@qq.com>
2022-10-21 08:35:58 +0800

    modify

--- src/main/java/com/yxy/nova/netty/udp/UdpHandler.java
+++ src/main/java/com/yxy/nova/netty/udp/UdpHandler.java
@@ -1,7 +1,5 @@
 package com.yxy.nova.netty.udp;
 
-import com.yxy.nova.mwh.retry.api.HandlerStatus;
-import com.yxy.nova.mwh.retry.api.RetryTaskExecutionContext;
 import com.yxy.nova.nio.UDPMessage;
 
 public interface UdpHandler {
