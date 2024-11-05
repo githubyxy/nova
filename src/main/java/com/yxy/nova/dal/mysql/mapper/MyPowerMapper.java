5244cb770439f76f7205d0ca372db35b009eb680
yuxiaoyu <2416175338@qq.com>
2024-11-05 16:29:10 +0800

    优化

--- src/main/java/com/yxy/nova/dal/mysql/mapper/MyPowerMapper.java
+++
@@ -1,8 +0,0 @@
-package com.yxy.nova.dal.mysql.mapper;
-
-import com.yxy.nova.dal.mysql.dataobject.MyPowerDO;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.special.InsertListMapper;
-
-public interface MyPowerMapper extends Mapper<MyPowerDO>, InsertListMapper<MyPowerDO> {
-}
--- src/main/java/com/yxy/nova/dal/mysql/mapper/TaskItemExecCallMapper.java
+++
@@ -1,8 +0,0 @@
-package com.yxy.nova.dal.mysql.mapper;
-
-import com.yxy.nova.dal.mysql.dataobject.TaskItemExecCallDO;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.special.InsertListMapper;
-
-public interface TaskItemExecCallMapper extends Mapper<TaskItemExecCallDO>, InsertListMapper<TaskItemExecCallDO> {
-}
