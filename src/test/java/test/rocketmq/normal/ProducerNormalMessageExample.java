45df13e77a2c5e37375e88f476f01c8ad2ae1cb7
yuxiaoyu <2416175338@qq.com>
2024-11-11 17:52:37 +0800

    rmq 简单消息测试

--- src/test/java/test/rocketmq/normal/ProducerNormalMessageExample.java
+++ src/test/java/test/rocketmq/normal/ProducerNormalMessageExample.java
@@ -25,15 +25,18 @@ package test.rocketmq.normal;
  * limitations under the License.
  */
 
-import java.nio.charset.StandardCharsets;
+import com.yxy.nova.mwh.utils.time.DateTimeUtil;
+import lombok.SneakyThrows;
 import org.apache.rocketmq.client.apis.ClientException;
 import org.apache.rocketmq.client.apis.ClientServiceProvider;
 import org.apache.rocketmq.client.apis.message.Message;
 import org.apache.rocketmq.client.apis.producer.Producer;
 import org.apache.rocketmq.client.apis.producer.SendReceipt;
-import org.apache.rocketmq.client.java.example.ProducerSingleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import test.rocketmq.YxyProducerSingleton;
+
+import java.nio.charset.StandardCharsets;
 
 public class ProducerNormalMessageExample {
     private static final Logger log = LoggerFactory.getLogger(ProducerNormalMessageExample.class);
@@ -41,31 +44,42 @@ public class ProducerNormalMessageExample {
     private ProducerNormalMessageExample() {
     }
 
+    @SneakyThrows
     public static void main(String[] args) throws ClientException {
         final ClientServiceProvider provider = ClientServiceProvider.loadService();
 
-        String topic = "yourNormalTopic";
-        final Producer producer = ProducerSingleton.getInstance(topic);
-        // Define your message body.
-        byte[] body = "This is a normal message for Apache RocketMQ".getBytes(StandardCharsets.UTF_8);
-        String tag = "yourMessageTagA";
-        final Message message = provider.newMessageBuilder()
-                // Set topic for the current message.
-                .setTopic(topic)
-                // Message secondary classifier of message besides topic.
-                .setTag(tag)
-                // Key(s) of the message, another way to mark message besides message id.
-                .setKeys("yourMessageKey-1c151062f96e")
-                .setBody(body)
-                .build();
-        try {
-            final SendReceipt sendReceipt = producer.send(message);
-            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
-        } catch (Throwable t) {
-            log.error("Failed to send message", t);
+        String topic = "TestTopic";
+        final Producer producer = YxyProducerSingleton.getInstance(topic);
+
+        while (true) {
+            Thread.sleep(10000);
+
+            // Define your message body.
+            String msg = "测试rocketmq消息" + DateTimeUtil.datetime18();
+            byte[] body = msg.getBytes(StandardCharsets.UTF_8);
+//            String tag = Math.random() > 0.5D ? "tagA" : "tagB";
+            String tag = "tagB";
+            final Message message = provider.newMessageBuilder()
+                    // Set topic for the current message.
+                    .setTopic(topic)
+                    // Message secondary classifier of message besides topic.
+                    .setTag(tag)
+                    // Key(s) of the message, another way to mark message besides message id.
+//                    .setKeys("yourMessageKey-1c151062f96e")
+                    .setBody(body)
+                    .build();
+
+            try {
+                final SendReceipt sendReceipt = producer.send(message);
+                System.out.println("Send message successfully, tag=" + tag + ", msg=" + msg);
+            } catch (Throwable t) {
+                log.error("Failed to send message", t);
+            }
+            // Close the producer when you don't need it anymore.
+            // You could close it manually or add this into the JVM shutdown hook.
+            // producer.close();
         }
-        // Close the producer when you don't need it anymore.
-        // You could close it manually or add this into the JVM shutdown hook.
-        // producer.close();
+
+
     }
 }
